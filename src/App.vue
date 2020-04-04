8615bf62443e87fb76661cc8467485b6f5705c1c
Ashfaqul-Asif <ashfaqulasif41@gmail.com>
2020-04-04 18:32:35 +0600

    error

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+      <ejs-documenteditor> </ejs-documenteditor>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Vue from 'vue';
+import { DocumentEditorPlugin } from '@syncfusion/ej2-vue-documenteditor';
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+Vue.use(DocumentEditorPlugin);
+export default {
+  data () {
+    return {
     }
   }
 }
+</script>
+
+<style>
+ 
+ 
+
+@import "../../node_modules/@syncfusion/ej2-vue-documenteditor/styles/material.css";
 </style>
